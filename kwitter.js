b3d5b2c81da823427957b26e1c9214693502de7f
Prof Monaliza <83433815+profmonaliza@users.noreply.github.com>
2023-03-20 07:18:53 -0300

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,13 @@
+//Coloque o nome da função que acontece quando botão na linha 41 for clicado(linha 41, index.html)
+function ???() {
+
+  //Utilize o código que pega o elemento pelo id
+  user_name = ???("user_name").value;
+
+  //Utilize o código 'setItem', pois ele irá enviar o nome do usuário para o localStorage
+  localStorage.???("user_name", user_name);
+  
+  //Utilize o código 'window.location', pois ele altera de tela.
+    ??? = "kwitter_room.html";
+}
+
