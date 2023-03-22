fb67dd2b5aeb202037f475d7694d1c906b38c1e2
Prof Monaliza <83433815+profmonaliza@users.noreply.github.com>
2023-03-22 18:17:57 -0300

    Update kwitter.js

--- kwitter.js
+++ kwitter.js
@@ -1,13 +1,10 @@
-//Coloque o nome da função que acontece quando botão na linha 41 for clicado(linha 41, index.html)
-function ???() {
 
-  //Utilize o código que pega o elemento pelo id
-  user_name = ???("user_name").value;
+function addUser() {
 
-  //Utilize o código 'setItem', pois ele irá enviar o nome do usuário para o localStorage
-  localStorage.???("user_name", user_name);
+  user_name = document.getElementById("user_name").value;
+
+  localStorage.setItem("user_name", user_name);
   
-  //Utilize o código 'window.location', pois ele altera de tela.
-    ??? = "kwitter_room.html";
+    window.location = "kwitter_room.html";
 }
 
